eb4881344e620019ccdc29602c6b2aa1c7a62714
FHGDev <gideon.lingle.dev@gmail.com>
2020-06-19 17:11:10 -0500

    Update

--- bot.js
+++ bot.js
@@ -2,3 +2,9 @@ require('dotenv').config()
 
 const discord = require('discord.js')
 const bot = new discord.Client()
+
+bot.on('ready', () => {
+    console.log(`${bot.user.username} is ready!`)
+})
+
+bot.login(process.env.token)
